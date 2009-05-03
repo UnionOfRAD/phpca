232fba8f2d7f37bb60a33d04cfda3bfbecfab6b0
Stefan Priebsch <stefan.priebsch@e-novative.de>
2009-05-03 23:23:00 +0200

    Added missing test cases.

---
+++ PHPCA/Tests/MessageTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright (c) 2009 Stefan Priebsch <stefan@priebsch.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ * 
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of Stefan Priebsch nor the names of contributors
+ *     may be used to endorse or promote products derived from this software 
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER ORCONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPca
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>
+ * @license    BSD License
+ */
+
+namespace spriebsch\PHPca\Tests;
+
+use spriebsch\PHPca\Message as Message;
+use spriebsch\PHPca\Token as Token;
+
+require_once 'PHPUnit/Framework.php';
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+/**
+ * Tests for the Message class.
+ *
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>
+ */
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+    * @covers spriebsch\PHPca\Message:getMessage
+    */
+    public function testGetMessage()
+    {
+        $msg = new Message('filename', 'message text');
+
+        $this->assertEquals('message text', $msg->getMessage());
+    }
+
+    /**
+    * @covers spriebsch\PHPca\Message:getColumn
+    */
+    public function testGetColumn()
+    {
+        $t = new Token(367, "<?php", 0, 2, 0);
+        $msg = new Message('filename', 'message text', $t);
+
+        $this->assertEquals(2, $msg->getColumn());
+    }
+}
+?>
