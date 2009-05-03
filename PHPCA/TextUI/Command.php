2371ae920b9b8bd9abe6f1c3c69efec922029712
Stefan Priebsch <stefan.priebsch@e-novative.de>
2009-05-03 23:06:41 +0200

    Reformatted.

--- PHPCA/TextUI/Command.php
+++ PHPCA/TextUI/Command.php
@@ -45,72 +45,68 @@ namespace spriebsch\PHPca;
  */
 class Command
 {
-  protected $version = '0.2.0';
+    protected $version = '0.2.0';
 
-  protected $path;
+    protected $path;
 
-  /**
-   * Path to a directory containing the rules.
-   */
-  protected $rulePath = 'PHPCA/Rules';
+    /**
+    * Path to a directory containing the rules.
+    */
+    protected $rulePath = 'PHPCA/Rules';
 
-  protected $phpExecutable;
+    protected $phpExecutable;
 
-  protected $positionCount = 0;
+    protected $positionCount = 0;
 
-  protected $files;
+    protected $files;
 
-  protected $result;
+    protected $result;
 
+    protected function loadRules()
+    {
+        $list = array();
 
-  protected function loadRules()
-  {
-    $list = array();
+        $it = new \DirectoryIterator($this->rulePath);
 
-    $it = new \DirectoryIterator($this->rulePath);
+        foreach ($it as $file) {
+            if (!$file->isFile()) {
+                continue;
+            }
 
-    foreach ($it as $file) {
-      if (!$file->isFile()) {
-        continue;
-      }
+            if (substr($file->getPathname(), -4) != '.php') {
+                continue;
+            }
 
-      if (substr($file->getPathname(), -4) != '.php') {
-        continue;
-      }
+            $classname = __NAMESPACE__ . '\\' . substr($file->getFilename(), 0, -4);
 
-      $classname = __NAMESPACE__ . '\\' . substr($file->getFilename(), 0, -4);
+            require_once $this->rulePath . DIRECTORY_SEPARATOR . $file->getFilename();
 
-      require_once $this->rulePath . DIRECTORY_SEPARATOR . $file->getFilename();
+            if (!class_exists($classname)) {
+                throw new Exception('File ' . $file->getFilename() . ' does not contain rule class ' . $classname);
+            }
 
-      if (!class_exists($classname)) {
-        throw new Exception('File ' . $file->getFilename() . ' does not contain rule class ' . $classname);
-      }
+            $list[] = new $classname;
+        }
 
-      $list[] = new $classname;
+        return $list;
     }
 
-    return $list;
-  }
-
-
-  protected function printUsage()
-  {
-    print 'Usage: phpca -p path_to_php <file to analyze>' . PHP_EOL .
-          '       phpca -p path_to_php <directory to analyze>' . PHP_EOL . PHP_EOL;
-  }
-
-
-  protected function printLetter($letter = '.')
-  {
-    if ($this->positionCount > 58) {
-      echo PHP_EOL;
-      $this->positionCount = 0;
+    protected function printUsage()
+    {
+        print 'Usage: phpca -p path_to_php <file to analyze>' . PHP_EOL .
+              '       phpca -p path_to_php <directory to analyze>' . PHP_EOL . PHP_EOL;
     }
 
-    echo $letter;
-    $this->positionCount++;
-  }
+    protected function printLetter($letter = '.')
+    {
+        if ($this->positionCount > 58) {
+            echo PHP_EOL;
+            $this->positionCount = 0;
+        }
 
+        echo $letter;
+        $this->positionCount++;
+    }
 
     protected function parseCommandLine($arguments)
     {
@@ -141,114 +137,110 @@ class Command
 
         // the last argument is the file or directory name
         $this->path = $argument;
-  }
-
-
-  protected function checkSettings()
-  {
-      if ($this->path == '') {
-          throw new Exception('Missing argument: no file or directory to analyze');
-      }
-
-      if ($this->phpExecutable == '') {
-          throw new Exception('Missing argument: path to PHP executable (-p) must be specified');
-      }
-  }
-
-
-  protected function doRun()
-  {
-    Constants::init();
-
-    $linter = new Linter($this->phpExecutable);
-    $linter->checkPhpBinary();
-
-    $tokenizer = new Tokenizer();
-    $rules = $this->loadRules();
-
-    $this->result = new Result();
-
-    $fileList = new FileList();
-
-    foreach ($fileList->listFiles($this->path) as $file) {
-
-      $this->result->addFile($file);
-
-      $lintResult = $linter->check($file);
-
-      if ($lintResult != '') {
-        $this->printLetter('E');
-        $this->result->addMessage(new LintError($file, strstr($lintResult, PHP_EOL, true)));
-        continue;
-      }
-
-      $tokenizedFile = $tokenizer->tokenize($file, file_get_contents($file));
-
-      foreach ($rules as $rule) {
-        $tokenizedFile->rewind();
-        $rule->check($tokenizedFile, $this->result);
-      }
-
-      if ($this->result->hasErrors($file)) {
-        $this->printLetter('F');
-      } else {
-        $this->printLetter();
-      }
     }
-  }
 
-
-  protected function printSummary()
-  {
-    echo PHP_EOL . PHP_EOL;
-
-    if (!$this->result->hasErrors()) {
-      echo 'OK';
-    } else {
-
-      foreach($this->result->getFiles() as $file) {
-        if ($this->result->hasErrors($file)) {
-          echo $file . ':' . PHP_EOL;
-          foreach ($this->result->getErrors($file) as $error) {
-            if ($error instanceOf LintError) {
-              echo $error->getMessage() . PHP_EOL;
-            } else {
-              echo 'Line ' . $error->getLine() . ', column ' . $error->getColumn() . ': Error: ' . $error->getMessage() . PHP_EOL;
-            }
-          }
-          echo PHP_EOL;
+    protected function checkSettings()
+    {
+        if ($this->path == '') {
+            throw new Exception('Missing argument: no file or directory to analyze');
         }
-      }
 
-      echo 'FAIL';
+        if ($this->phpExecutable == '') {
+            throw new Exception('Missing argument: path to PHP executable (-p) must be specified');
+        }
     }
 
-    echo ' (';
-    echo $this->result->getNumberOfFiles() . ' files, ';
-    echo $this->result->getNumberOfErrors() . ' errors, ';
-    echo $this->result->getNumberOfWarnings() . ' warnings';
-    echo ')';
+    protected function doRun()
+    {
+        Constants::init();
 
-    echo PHP_EOL . PHP_EOL;
-  }
+        $linter = new Linter($this->phpExecutable);
+        $linter->checkPhpBinary();
 
+        $tokenizer = new Tokenizer();
+        $rules = $this->loadRules();
 
-  public function run($arguments)
-  {
-    echo 'PHP Code Analyzer ' . $this->version . ' by Stefan Priebsch' . PHP_EOL . PHP_EOL;
+        $this->result = new Result();
 
-    try {
-      $this->parseCommandLine($arguments);
-      $this->checkSettings();
-      $this->doRun();
-      $this->printSummary();
+        $fileList = new FileList();
+
+        foreach ($fileList->listFiles($this->path) as $file) {
+
+            $this->result->addFile($file);
+
+            $lintResult = $linter->check($file);
+
+            if ($lintResult != '') {
+                $this->printLetter('E');
+                $this->result->addMessage(new LintError($file, strstr($lintResult, PHP_EOL, true)));
+                continue;
+            }
+
+            $tokenizedFile = $tokenizer->tokenize($file, file_get_contents($file));
+
+            foreach ($rules as $rule) {
+                $tokenizedFile->rewind();
+                $rule->check($tokenizedFile, $this->result);
+            }
+
+            if ($this->result->hasErrors($file)) {
+                $this->printLetter('F');
+            } else {
+                $this->printLetter();
+            }
+        }
     }
 
-    catch (Exception $e) {
-      echo 'Error: ' . $e->getMessage() . PHP_EOL . PHP_EOL;
-      $this->printUsage();
-      exit();
+    protected function printSummary()
+    {
+        echo PHP_EOL . PHP_EOL;
+
+        if (!$this->result->hasErrors()) {
+            echo 'OK';
+        } else {
+
+            foreach($this->result->getFiles() as $file) {
+                if ($this->result->hasErrors($file)) {
+                    echo $file . ':' . PHP_EOL;
+                    foreach ($this->result->getErrors($file) as $error) {
+                        if ($error instanceOf LintError) {
+                            echo $error->getMessage() . PHP_EOL;
+                        } else {
+                            echo 'Line ' . $error->getLine() . ', column ' . $error->getColumn() . ': Error: ' . $error->getMessage() . PHP_EOL;
+                        }
+                    }
+                    echo PHP_EOL;
+                }
+            }
+
+            echo 'FAIL';
+        }
+
+        echo ' (';
+        echo $this->result->getNumberOfFiles() . ' files, ';
+        echo $this->result->getNumberOfErrors() . ' errors, ';
+        echo $this->result->getNumberOfWarnings() . ' warnings';
+        echo ')';
+
+        echo PHP_EOL . PHP_EOL;
     }
-  }
+
+    public function run($arguments)
+    {
+        echo 'PHP Code Analyzer ' . $this->version . ' by Stefan Priebsch' . PHP_EOL . PHP_EOL;
+
+        try {
+            $this->parseCommandLine($arguments);
+            $this->checkSettings();
+            $this->doRun();
+            $this->printSummary();
+        }
+
+        catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage() . PHP_EOL . PHP_EOL;
+            $this->printUsage();
+            exit();
+        }
+    }
 }
 ?>
