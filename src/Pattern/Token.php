2a51819b3ea9e4d74f68bd1349747ac351c69543
Stefan Priebsch <steve@NCC-1701.(none)>
2009-07-25 19:44:10 +0200

    Removed obsolete comment.

--- src/Pattern/Token.php
+++ src/Pattern/Token.php
@@ -44,7 +44,6 @@ use spriebsch\PHPca\Constants;
  *
  * @author     Stefan Priebsch <stefan@priebsch.de>
  * @copyright  Stefan Priebsch <stefan@priebsch.de>. All rights reserved.
- * @todo RENAME THIS CLASS
  */
 class Token implements PatternInterface
 {
