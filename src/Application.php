4ef21ed5bc3eb860d6970231996055462b9a66db
Stefan Priebsch <steve@NCC-1701.(none)>
2009-07-29 10:28:29 +0200

    Added Application class.

---
+++ src/Application.php
@@ -0,0 +1,313 @@
+<?php
+/**
+ * Copyright (c) 2009 Stefan Priebsch <stefan@priebsch.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of Stefan Priebsch nor the names of contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER ORCONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPca
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>. All rights reserved.
+ * @license    BSD License
+ */
+
+namespace spriebsch\PHPca;
+
+/**
+ * The PHPCA application.
+ *
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>. All rights reserved.
+ */
+class Application
+{
+    /**
+     * @var string
+     */
+    static public $version = '0.3.0';
+
+    /**
+     * @var array of string
+     */
+    protected $rulePaths = array();
+
+    /**
+     * @var array of Rule
+     */
+    protected $rules = array();
+
+    /**
+     * @var array of string
+     */
+    protected $disabledRuleNames = array();
+
+    /**
+     * @var Result
+     */
+    protected $result;
+
+    /**
+     * @var ProgressPrinterInterface
+     */
+    protected $progressPrinter;
+
+    /**
+     * @var bool
+     */
+    protected $enableBuiltInRules = true;
+    
+    /**
+     * Returns array of all .php filenames in given directory.
+     * If $path points to a single file, we do not iterate.
+     *
+     * @param string $path
+     * @return array
+     */
+    protected function listFiles($path)
+    {
+        if (is_file($path)) {
+            return array($path);
+        }
+
+        $result = array();
+
+        if (!file_exists($path)) {
+            throw new Exception($path . ' does not exist');
+        }
+
+        $it = new PhpFileFilterIterator(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path)));
+
+        foreach ($it as $file) {
+            $result[] = $file->getPathname();
+        }
+
+        return $result;
+    }
+
+    /**
+     * Converts a rule filename to a rule classname by removing .php
+     *
+     * @param string $fileName The file name
+     * @return string The rule name
+     */
+    protected function toClassName($fileName)
+    {
+        return '\\spriebsch\\PHPca\\Rule\\' . substr(basename($fileName), 0, -4);
+    }
+
+    /**
+     * Load all rules from given directories
+     * Note: built-in rules are listed in the $_ClassMap.
+     * Returns an array of Rule object instances.
+     *
+     * @return array
+     * @todo handle potential class name conflicts between built-in and custom rules
+     */
+    protected function loadRules()
+    {
+        $result = array();
+
+        if ($this->enableBuiltInRules) {
+
+            $builtInRules = $this->listFiles(__DIR__ . '/Rule');
+
+            foreach ($builtInRules as $rule) {
+                $className = $this->toClassName($rule);
+
+                if (!in_array($className, $this->disabledRuleNames)) {
+                    $result[] = new $className;
+                }
+            }
+        }
+
+        foreach ($this->rulePaths as $path) {
+
+            $rules = $this->listFiles($path);
+
+            foreach($rules as $rule) {
+                $className = $this->toClassName($rule);
+
+                if (!in_array($className, $this->disabledRuleNames)) {
+                    require_once $rule;
+                    $result[] = new $className;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Enforce all rules
+     *
+     * @param string $fileName Filename
+     * @param File $file
+     * @return void
+     */
+    protected function enforceRules($fileName, File $file)
+    {
+        if (sizeof($this->rules) == 0) {
+            throw new Exception('No rules to enforce');
+        }
+
+        foreach ($this->rules as $rule) {
+            try {
+                $rule->check($file, $this->result);
+            }
+
+            catch (Exception $e) {
+                $this->result->addMessage(new RuleError($fileName, 'Rule ' . get_class($rule) . ': ' . $e->getMessage()));
+            }
+        }
+    }
+
+    /**
+     * Disable a rule
+     *
+     * @param string $name Rule Name
+     */
+    public function disableRule($name)
+    {
+        $this->disabledRuleNames[] = $name;
+    }
+
+    /**
+     * Toggle use of built-in rules.
+     * Default value is true.
+     *
+     * @param bool $flag Flag
+     * @return void
+     */
+    public function setEnableBuiltInRules($flag)
+    {
+        if (!is_bool($flag)) {
+            throw new Exception('Boolean value expected');
+        }
+
+        $this->enableBuiltInRules = $flag;
+    }
+
+    /**
+     * Add a path to load rules from.
+     *
+     * @param string $path
+     * @return array
+     */
+    public function addRulePath($path)
+    {
+        if (!file_exists($path)) {
+            throw new Exception('The path ' . $path . ' does not exist');
+        }
+
+        // Make sure each item of the rule path is unique
+        if (!in_array($path, $this->rulePaths)) {
+            $this->rulePaths[] = $path;
+        }
+    }
+
+    /**
+     * Return an array of Rule types.
+     *
+     * @return array
+     */
+    public function getRulePaths()
+    {
+        return $this->rulePaths;
+    }
+
+    /**
+     * Register a callback that is notified whenever a file has been processed.
+     * Can be used to display a dot, E of F for each processed file in console mode.
+     *
+     * @param object
+     * @return void
+     */
+    public function registerProgressPrinter(ProgressPrinterInterface $progressPrinter)
+    {
+        $this->progressPrinter = $progressPrinter;
+    }
+
+    /**
+     * PHPca's main method. Returns a result object holding
+     * error and warning messages for all the files that have been analyzed.
+     *
+     * @param string $pathToPhpExecutable path to PHP executable for lint check
+     * @param string $fileOrDirectory     path to file or directory to check
+     * @return object
+     */
+    public function run($pathToPhpExecutable, $fileOrDirectory)
+    {
+        if ($pathToPhpExecutable == '') {
+            throw new Exception('No path to PHP executable specified');
+        }
+
+        if ($fileOrDirectory == '') {
+            throw new Exception('No file or directory to analyze');
+        }
+
+        // Define our own additionl T_* token constants
+        Constants::init();
+
+        // Set up the lint checker and make sure that given path points to a PHP binary
+        $linter = new Linter($pathToPhpExecutable);
+
+        // Create result object that collects the error and warning messages
+        $this->result = new Result();
+
+        // Create a list of all rules to enforce
+        $this->rules = $this->loadRules();
+
+        // List all PHP files in given path
+        $phpFiles = $this->listFiles($fileOrDirectory);
+
+        if (sizeof($phpFiles) == 0) {
+            throw new Exception('No PHP files to analyze');
+        }
+
+        foreach ($phpFiles as $phpFile) {
+
+            // Remember that we have processed this file,
+            // even if it generates no message at all.
+            $this->result->addFile($phpFile);
+
+            if ($linter->runLintCheck($phpFile)) {
+                $this->enforceRules($phpFile, Tokenizer::tokenize($phpFile, file_get_contents($phpFile)));
+            } else {
+                $this->result->addMessage(new LintError($phpFile, $linter->getErrorMessages()));
+            }
+
+            // Notify the progress printer that we have analyzed a file
+            if (is_object($this->progressPrinter)) {
+                $this->progressPrinter->showProgress($phpFile, $this->result);
+            }
+        }
+
+        // Return the result object containing all error and warning messages
+        return $this->result;
+    }
+}
+?>
---
+++ tests/ApplicationTest.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Copyright (c) 2009 Stefan Priebsch <stefan@priebsch.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   * Neither the name of Stefan Priebsch nor the names of contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER ORCONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPca
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>. All rights reserved.
+ * @license    BSD License
+ */
+
+namespace spriebsch\PHPca;
+
+require_once 'PHPUnit/Framework.php';
+require_once __DIR__ . '/../src/Exceptions.php';
+require_once __DIR__ . '/../src/Loader.php';
+
+/**
+ * Tests for the Application class.
+ *
+ * @author     Stefan Priebsch <stefan@priebsch.de>
+ * @copyright  Stefan Priebsch <stefan@priebsch.de>. All rights reserved.
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        Loader::init();
+        Loader::registerPath(__DIR__ . '/../src');
+
+        $this->application = new Application();
+        $this->application->setEnableBuiltInRules(false);
+    }
+
+    protected function tearDown()
+    {
+        Loader::reset();
+    }
+
+    /**
+     * Does not specify a PHP executable
+     * (which is required to run the lint checks).
+     *
+     * @covers spriebsch\PHPca\Application::run
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testRunThrowsExceptionWhenNoPhpExecutableIsGiven()
+    {
+        $application = new Application();
+        $application->run('', '');
+    }
+
+    /**
+     * Does not specify a file or directory to analyze.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testRunThrowsExceptionWhenNoPathToAnalyzeIsGiven()
+    {
+        $this->application->run('php', '');
+    }
+
+    /**
+     * Calls setEnableBuiltInRules with a non-boolean parameter.
+     *
+     * @covers spriebsch\PHPca\Application::enableBuiltInRules
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testSetEnableBuiltInRulesThrowsExceptionWhenParameterIsNotBoolean()
+    {
+        $this->application->setEnableBuiltInRules('nonsense');
+    }
+
+    /**
+     * Disables the built-in rules and do not specify any
+     * additional rule directories, thus there are no rules to enforce.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     * @covers spriebsch\PHPca\Application::enableBuiltInRules
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testRunThrowsExceptionWhenNoRulesToEnforce()
+    {
+        $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application');
+    }
+
+    /**
+     * Specifies a directory that contains no PHP files.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testRunThrowsExceptionWhenNoFilesToAnalyze()
+    {
+        $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/none');
+    }
+
+    /**
+     * Specifies a non-existing file/directory to analyze.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testRunThrowsExceptionWhenDirectoryDoesNotExist()
+    {
+        $this->application->run(trim(exec('which php')), '/nonsense');
+    }
+
+    /**
+     * Analyzes a file with a lint error.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     */
+    public function testRunCreatesErrorOnLintFail()
+    {
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/lint_fail');
+
+        $this->assertTrue($result->hasLintError(__DIR__ . '/_testdata/Application/lint_fail/fail.php'));
+    }
+
+    /**
+     * Calls run() with a single file instead of a directory.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     */
+    public function testRunAcceptsSingleFile()
+    {
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules/pass');
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/pass/pass.php');
+
+        $this->assertFalse($result->hasErrors());
+    }
+
+    /**
+     * Registers a progress printer to make sure that it gets called
+     * after processing a file.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     */
+    public function testRunNotifiesProgressPrinter()
+    {
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules/pass');
+
+        $mock = $this->getMock('spriebsch\PHPca\ProgressPrinterInterface', array('showProgress'));
+        $mock->expects($this->once())->method('showProgress');
+
+        $this->application->registerProgressPrinter($mock);
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/pass');
+    }
+
+    /**
+     * Calls run() with a single file instead of a directory.
+     * This test depends on the CloseTagAtEndRule and might fail if rules
+     * are added that fail on _testdata/Application/fail/fail.php
+     * or when the error message of CloseTagAtEndRule is modified.
+     *
+     * @covers spriebsch\PHPca\Application::run
+     */
+    public function testRunEnforcesBuiltInRules()
+    {
+        $this->application->setEnableBuiltInRules(true);
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/fail/fail.php');
+        $errors = $result->getErrors(__DIR__ . '/_testdata/Application/fail/fail.php');
+
+        $this->assertContains('File does not end with PHP close tag', $errors[0]->getMessage());
+    }
+
+    /**
+     * Calls addRulePath with a non-existing path.
+     *
+     * @covers spriebsch\PHPca\Application::addRulePath
+     * @expectedException spriebsch\PHPca\Exception
+     */
+    public function testAddRulePathThrowsExceptionWhenPathDoesNotExist()
+    {
+        $this->application->addRulePath('does/not/exist');
+    }
+
+    /**
+     * Calls addRulePath twíce with the same path,
+     * and makes sure that the path is only added once.
+     *
+     * @covers spriebsch\PHPca\Application::addRulePath
+     * @covers spriebsch\PHPca\Application::getRulePaths
+     */
+    public function testAddRulePathAddsPathOnlyOnce()
+    {
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules');
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules');
+
+        $this->assertEquals(array(__DIR__ . '/_testdata/Application/_rules'), $this->application->getRulePaths());
+    }
+
+    /**
+     * Registers ErrorTestRule and PassTestRule, but then explicitly disables
+     * ErrorTestRule so that no error must occur.
+     *
+     * @covers spriebsch\PHPca\Application::disableRule
+     * @covers spriebsch\PHPca\Application::enforceRules
+     */
+    public function testDisableRuleDisablesGivenRule()
+    {
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules/pass');
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules/error');
+        $this->application->disableRule('\\spriebsch\\PHPca\\Rule\\ErrorTestRule');
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/pass/pass.php');
+
+        $this->assertFalse($result->hasErrors());
+    }
+
+    /**
+     * Disables CloseTagAtEndRule rule, then analyzes fail.php that has
+     * no close tag at the end. If the rule has been disabled, no error
+     * may occur. Also see testRunLoadsBuiltInRules().
+     *
+     * @covers spriebsch\PHPca\Application::disableRule
+     * @covers spriebsch\PHPca\Application::enforceRules
+     */
+    public function testDisableRuleDisablesBuiltInRule()
+    {
+        $this->application->setEnableBuiltInRules(true);
+        $this->application->disableRule('\\spriebsch\\PHPca\\Rule\\CloseTagAtEndRule');
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/fail/fail.php');
+
+        $this->assertFalse($result->hasErrors());
+    }
+
+    /**
+     * Registes a rule that always throws an exception to make
+     * sure that a RuleError is generated.
+     *
+     * @covers spriebsch\PHPca\Application::enforceRules
+     */
+    public function testRunGeneratesRuleErrorOnExceptionInRule()
+    {
+        $this->application->addRulePath(__DIR__ . '/_testdata/Application/_rules/fail');
+
+        $result = $this->application->run(trim(exec('which php')), __DIR__ . '/_testdata/Application/pass/pass.php');
+        $errors = $result->getErrors(__DIR__ . '/_testdata/Application/pass/pass.php');
+
+        $this->assertTrue($result->hasErrors());
+        $this->assertTrue($errors[0] instanceOf RuleError);
+    }
+}
+?>
