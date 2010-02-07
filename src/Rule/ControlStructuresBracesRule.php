93029690293f8dbabc34e571c208dcba6f781dff
David Persson <davidpersson@gmx.de>
2010-02-07 13:32:08 +0100

    Tabs to spaces for ControlStructure rules.

--- src/Rule/ControlStructuresBracesRule.php
+++ src/Rule/ControlStructuresBracesRule.php
@@ -8,48 +8,50 @@ use spriebsch\PHPca\Token;
  * Ensures that the control structures have a space before the parenthesis
  * and a space between the parenthesis and the brace.
  */
-class ControlStructuresBracesRule extends Rule {
+class ControlStructuresBracesRule extends Rule
+{
 
-	protected $controlTokens = array(
-		T_IF,
-		T_WHILE,
-		T_FOREACH,
-		T_FOR,
-		T_SWITCH
-	);
+    protected $controlTokens = array(
+        T_IF,
+        T_WHILE,
+        T_FOREACH,
+        T_FOR,
+        T_SWITCH
+    );
 
-	/**
-	 * Performs the rule check
-	 *
-	 * @return void
-	 */
-	protected function doCheck() {
-		foreach ($this->controlTokens as $id) {
-			while ($this->file->seekTokenId($id)) {
-				$controlToken = $this->file->current();
-				$name = $controlToken->getText();
+    /**
+     * Performs the rule check
+     *
+     * @return void
+     */
+    protected function doCheck()
+    {
+        foreach ($this->controlTokens as $id) {
+            while ($this->file->seekTokenId($id)) {
+                $controlToken = $this->file->current();
+                $name = $controlToken->getText();
 
-				if ($this->file->seekTokenId(T_OPEN_CURLY)) {
-					$curlyToken = $this->file->current();
+                if ($this->file->seekTokenId(T_OPEN_CURLY)) {
+                    $curlyToken = $this->file->current();
 
-					if (!$this->file->valid() || $controlToken->getLine() != $curlyToken->getLine()) {
-						if ($controlToken->getLine() == $curlyToken->getLine() - 1) {
-							$this->addViolation("Brace for `{$name}` statement on wrong line", $curlyToken);
-						} else {
-							$this->addViolation("No brace for `{$name}` statement", $controlToken);
-							$this->file->seekToken($controlToken);
-						}
-					}
-				} else {
-					$this->addViolation("No brace for `{$name}` statement", $controlToken);
-					$this->file->seekToken($controlToken);
-				}
+                    if (!$this->file->valid() || $controlToken->getLine() != $curlyToken->getLine()) {
+                        if ($controlToken->getLine() == $curlyToken->getLine() - 1) {
+                            $this->addViolation("Brace for `{$name}` statement on wrong line", $curlyToken);
+                        } else {
+                            $this->addViolation("No brace for `{$name}` statement", $controlToken);
+                            $this->file->seekToken($controlToken);
+                        }
+                    }
+                } else {
+                    $this->addViolation("No brace for `{$name}` statement", $controlToken);
+                    $this->file->seekToken($controlToken);
+                }
 
-				$this->file->next();
-			}
-			$this->file->rewind();
-		}
-	}
+                $this->file->next();
+            }
+            $this->file->rewind();
+        }
+    }
 }
 
 ?>
--- src/Rule/ControlStructuresSpacingRule.php
+++ src/Rule/ControlStructuresSpacingRule.php
@@ -8,53 +8,55 @@ use spriebsch\PHPca\Token;
  * Ensures that the control structures have a space before the parenthesis
  * and a space between the parenthesis and the brace.
  */
-class ControlStructuresSpacingRule extends Rule {
+class ControlStructuresSpacingRule extends Rule
+{
 
-	protected $controlTokens = array(
-		T_IF,
-		T_WHILE,
-		T_FOREACH,
-		T_FOR,
-		T_SWITCH
-	);
+    protected $controlTokens = array(
+        T_IF,
+        T_WHILE,
+        T_FOREACH,
+        T_FOR,
+        T_SWITCH
+    );
 
-	/**
-	 * Performs the rule check
-	 *
-	 * @return void
-	 */
-	protected function doCheck() {
-		foreach ($this->controlTokens as $id) {
-			while ($this->file->seekTokenId($id)) {
-				$controlToken = $this->file->current();
-				$name = $controlToken->getText();
-				$this->file->next();
-				$token = $this->file->current();
+    /**
+     * Performs the rule check
+     *
+     * @return void
+     */
+    protected function doCheck()
+    {
+        foreach ($this->controlTokens as $id) {
+            while ($this->file->seekTokenId($id)) {
+                $controlToken = $this->file->current();
+                $name = $controlToken->getText();
+                $this->file->next();
+                $token = $this->file->current();
 
-				if ($token->getId() != T_WHITESPACE) {
-					$this->addViolation("No space after `{$name}`", $controlToken);
-					$this->file->prev();
-				}
+                if ($token->getId() != T_WHITESPACE) {
+                    $this->addViolation("No space after `{$name}`", $controlToken);
+                    $this->file->prev();
+                }
 
-				if ($this->file->seekTokenId(T_OPEN_CURLY)) {
-					$curlyToken = $this->file->current();
+                if ($this->file->seekTokenId(T_OPEN_CURLY)) {
+                    $curlyToken = $this->file->current();
 
-					if ($controlToken->getLine() == $curlyToken->getLine()) {
-						$this->file->prev();
-						$token = $this->file->current();
+                    if ($controlToken->getLine() == $curlyToken->getLine()) {
+                        $this->file->prev();
+                        $token = $this->file->current();
 
-						if ($token->getId() != T_WHITESPACE) {
-							$this->addViolation("No space before brace in `{$name}` statement", $token);
-						}
-					}
-				}
+                        if ($token->getId() != T_WHITESPACE) {
+                            $this->addViolation("No space before brace in `{$name}` statement", $token);
+                        }
+                    }
+                }
 
-				$this->file->seekToken($controlToken);
-				$this->file->next();
-			}
-			$this->file->rewind();
-		}
-	}
+                $this->file->seekToken($controlToken);
+                $this->file->next();
+            }
+            $this->file->rewind();
+        }
+    }
 }
 
 ?>
