7ffefe850f6b7f9605ff9d99c2958bd69ebbdab1
Stefan Priebsch <stefan@priebsch.de>
2010-02-13 12:48:55 +0100

    Switched to theseer's Autoload. Adapted build automation for Hudson CI.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../src/Exceptions.php';
-require_once __DIR__ . '/../src/Loader.php';
-
-require_once __DIR__ . '/Rule/AbstractRuleTest.php';
-
-spriebsch\PHPca\Loader::init();
-spriebsch\PHPca\Loader::registerPath(realpath(__DIR__ . '/../src'));
-spriebsch\PHPca\Loader::registerPath(realpath(__DIR__ . '/_testdata/Rule'));
+require_once __DIR__ . '/../src/Autoload.php';
+require_once __DIR__ . '/Autoload.php';
 
 ?>
