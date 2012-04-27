d1e2d98cd95ef5ec13bf679ba1eb953c13538348
David Persson <davidpersson@gmx.de>
2012-04-27 13:59:24 +0200

    Updating usage of multibyte `strlen()` in `MaximumLineLengthRule`.
    Closes #8
    Adding `multibyte` setting to rule.
    When setting is enabled will *require* you to have a multibyte enabled
    strlen() otherwise will error out and skip rule.

--- src/Rule/MaximumLineLengthRule.php
+++ src/Rule/MaximumLineLengthRule.php
@@ -3,6 +3,7 @@
 namespace spriebsch\PHPca\Rule;
 
 use spriebsch\PHPca\Token;
+use Exception;
 
 /**
  * Ensures that no line is longer than a certain amount of characters.
@@ -16,23 +17,40 @@ class MaximumLineLengthRule extends Rule
      */
     protected function doCheck()
     {
+        $strlen = $this->_strlen();
+
         $lines = explode(
             stripcslashes($this->configuration->getLineEndings()),
             $this->file->getSourceCode()
         );
         foreach ($lines as $i => $line) {
-            $length = mb_strlen($line, 'UTF8');
+            $length = $strlen($line);
             $tabs = array();
             $tabs = preg_match_all("/\t/", $line, $tabs);
             $length += ($tabs * 3);
 
             if ($length > $this->settings['line_length']) {
                 $this->addViolation(
-                    'Maximum line length exceeded',
+                    "Maximum line length exceeded",
                     null, $i + 1, $this->settings['line_length'] + 1
                 );
             }
         }
     }
+
+    protected function _strlen() {
+        if ($this->getSetting('multibyte')) {
+            if (extension_loaded('intl')) {
+                return function($string) { return grapheme_strlen($string); };
+            } elseif (extension_loaded('mbstring')) {
+                return function($string) { return mb_strlen($string, 'UTF-8'); };
+            }
+            $message  = 'No multibyte enabled `strlen()` function available. ';
+            $message .= 'Install the `intl` or `mbstring` extension.';
+            throw new Exception($message);
+        }
+        return function($string) { return strlen($string); };
+    }
 }
+
 ?>
